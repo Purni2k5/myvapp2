66f00ad181c97f5d1471286b2a3e8fac5e0ea914
Chef Dennis Barimah <chef.barimah@E0392692.mshome.net>
2018-06-20 16:17:11 +0000

    Gave another line for the label of login header

---
+++ My Vodafone/My Vodafone/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  My Vodafone
+//
+//  Created by Chef Dennis Barimah on 20/06/2018.
+//  Copyright © 2018 Chef Dennis Barimah. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginHeader: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        loginHeader.text = "Log in to \nMy Vodafone"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
