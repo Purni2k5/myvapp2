2689e6ccee3cd2fd996d49f0125b03a05e4c379d
Chef Work <dennis.barimah@vodafone.com>
2018-07-04 10:08:58 +0000

    Created topping up view controller

---
+++ My Vodafone/My Vodafone/toppingUpViewController.swift
@@ -0,0 +1,45 @@
+//
+//  toppingUpViewController.swift
+//  My Vodafone
+//
+//  Created by Chef Dennis Barimah on 04/07/2018.
+//  Copyright © 2018 Chef Dennis Barimah. All rights reserved.
+//
+
+import UIKit
+
+class toppingUpViewController: UIViewController {
+
+    @IBOutlet weak var btnClose: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // change close btn colour.
+        changeColour()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //change close btn colour
+    func changeColour(){
+        let close_image = UIImage(named: "new_close")
+        let tintedImage = close_image?.withRenderingMode(.alwaysTemplate)
+        btnClose.setImage(tintedImage, for: .normal)
+        btnClose.tintColor = UIColor.white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
