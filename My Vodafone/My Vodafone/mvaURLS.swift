32a667cefa1bc3e5af599bd8ae452fe6cb35f116
Chef Work <dennis.barimah@vodafone.com>
2018-07-24 18:02:47 +0000

    Added URL extension

---
+++ My Vodafone/My Vodafone/mvaURLS.swift
@@ -0,0 +1,14 @@
+//
+//  mvaURLS.swift
+//  My Vodafone
+//
+//  Created by Chef Dennis Barimah on 24/07/2018.
+//  Copyright © 2018 Chef Dennis Barimah. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    
+    static let offers = "https://myvodafoneappmw.vodafone.com.gh/MyVodafoneAPI/DBEnpoints"
+}
